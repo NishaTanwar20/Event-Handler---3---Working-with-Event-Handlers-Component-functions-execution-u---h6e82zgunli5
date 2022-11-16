dbefef492c63ac5ae5b1afde468884d60caaeba8
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-16 10:07:04 +0530

    onChange method added

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
+import {useState} from 'react';
 const App = () => {
-
+  const [message , setMessage] = useState('');
   const handleInput = (event) =>{
    // use console.log
+   setMessage(event.target.value);
+   console.log(event.target.value);
   }
 
   // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <input id="text-input" type={'text'} onChange={handleInput}/>
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
+      <input id="num-input"  type={'number'} onChange={handleInput}/>
       <br/>
     </div>
   )
